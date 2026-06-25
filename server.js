85877eaa846ef4c1b9ced9d4a042707c97acf020
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-25 19:32:15 +0500

    Set up Express server with task routes

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
 
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Task Manager API Running",
+  });
+});
+
+app.use("/api/tasks", taskRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
